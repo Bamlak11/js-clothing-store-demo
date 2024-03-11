7f7eee89e389b25a9fa99c3cc51fba5707323091
Bamlak11 <77420100+Bamlak11@users.noreply.github.com>
2024-03-11 14:24:24 -0500

    completed js tasks

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,13 @@
 // Task 1: Implement a function that calculates total sales
 
+function totalSales(sales){
+    let total = 0;
+    for (const sale of sales){
+        total += sale.amount;
+    }
+    return total;
+    // console should see 100 total
+}
 
 // Example Problem:
 const sales = [
@@ -11,7 +19,15 @@ const totalSales = calculateTotalSales(sales);
 console.log("Total Sales:", totalSales);
 
 // Task 2: Implement a function to filter out discounted items
-
+function filterDiscountedItems(items){
+    let regularPriceItems = [];
+    for( const item of items) {
+     if(!item.discounted){
+        regularPriceItems.push(item);
+     }
+    }
+    return regularPriceItems;
+}
 
 // Example Problem:
 const items = [
@@ -22,29 +38,57 @@ const items = [
 const regularPriceItems = filterDiscountedItems(items);
 console.log("Regular Price Items:", regularPriceItems);
 
+
 // Task 3: Define a class for Clothing Items
 
+class ClothingItem{
+    constructor(name, category, price){
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+}
 
 // Example Problem:
 const item = new ClothingItem("Dress", "Dresses", 45);
 console.log("Clothing Item:", item);
 
-// Task 4: Extend the ClothingItem class for Sale Items
 
+// Task 4: Extend the ClothingItem class for Sale Items
+class SaleItem extends ClothingItem{
+    constructor(name, category, price, discount){
+        super(name, category, price);
+        this.discount = discount;
+
+        }
+    }
 
 // Example Problem:
 const saleItem = new SaleItem("Jeans", "Bottoms", 60, 20);
 console.log("Sale Item:", saleItem);
 
-// Task 5: Create a custom constructor function for Clothing Stores
 
+// Task 5: Create a custom constructor function for Clothing Stores
+function ClothingStore(name, location){
+    let store = {};
+    store.name = name;
+    store.location = location;
+    return store;
+}
 
 // Example Problem:
 const store = ClothingStore("Fashion Boutique", "Main Street");
 console.log("Clothing Store:", store);
 
+
 // Task 6: Implement a class with default parameters for Clothing Brands
 
+class ClothingBrand{
+    constructor(name = "Unknown Brand", price = "$"){
+        this.name = name; 
+        this.price - price;
+    }
+}
 
 // Example Problem:
 const brand = new ClothingBrand();
